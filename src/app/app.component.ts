9e868ed5c3a1e22bd0fceb78eb9bbb49359c5e31
helloaliimran <hellolaiimran@yahoo.com>
2024-10-03 20:45:06 +0500

    inspire work initial commit angular theme install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FlexLayoutModule,MatButtonModule,MatIconModule,MatRadioModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'multi-theme-ui';
+  favoriteColor: string="dark";
+  theme = 'dark-theme';
+  colors:string[] = ['dark','light'];
+
+  toggleTheme(){
+    this.favoriteColor
+    const body = document.body;
+
+    if (body.classList.contains(this.theme)) {
+      body.classList.remove(this.theme);
+      }
+
+    this.theme = this.favoriteColor === 'light' ? 'light-theme' : 'dark-theme';
+   
+
+  // Add the new class
+  body.classList.add(this.theme);
+  }
 }
