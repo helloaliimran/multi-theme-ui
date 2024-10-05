4ff70d8d1d4f9ca1756264ecc71b62004b168f2f
helloaliimran <hellolaiimran@yahoo.com>
2024-10-05 16:15:19 +0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,48 @@
 import { Component } from '@angular/core';
-import {FlexLayoutModule } from '@angular/flex-layout';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatRadioModule} from '@angular/material/radio';
-import {FormsModule} from '@angular/forms';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FlexLayoutModule,MatButtonModule,MatIconModule,MatRadioModule,FormsModule],
+  imports: [FlexLayoutModule
+    , MatButtonModule
+    , MatIconModule
+    , MatRadioModule
+    , FormsModule
+    , MatSelectModule
+    , MatFormFieldModule
+    , MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'multi-theme-ui';
-  favoriteColor: string="dark";
+  favoriteColor: string = "dark";
   theme = 'dark-theme';
-  colors:string[] = ['dark','light'];
+  
 
-  toggleTheme(){
-    this.favoriteColor
+  myColors: any = [
+    { value: 'dark-theme', viewValue: 'dark' },
+    { value: 'light-theme', viewValue: 'light' },
+    { value: 'light-rose-theme', viewValue: 'light-rose' },
+    { value: 'dark-rose-theme', viewValue: 'dark-rose' },
+  ];
+
+  toggleTheme() {
+
     const body = document.body;
 
     if (body.classList.contains(this.theme)) {
       body.classList.remove(this.theme);
-      }
+    }
 
-    this.theme = this.favoriteColor === 'light' ? 'light-theme' : 'dark-theme';
-   
-
-  // Add the new class
-  body.classList.add(this.theme);
+    this.theme = this.favoriteColor;
+    body.classList.add(this.theme);
   }
 }
